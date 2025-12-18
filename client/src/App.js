30a8fb066cf567c2528c81962064531e9e75dedf
Yeinner20 <yeinner20@gmail.com>
2025-12-18 18:53:50 -0500

    Modificando la ruta URL

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 
-const API_URL = "https://mern-api-yeinner.onrender.com";
+const API_URL = "https://mern-api-yeinner.onrender.com/api/tasks";
 
 function App() {
   const [tasks, setTasks] = useState([]);
